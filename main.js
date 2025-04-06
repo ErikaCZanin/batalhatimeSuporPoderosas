f4e1ad5624ae12498347a7a67ffbda5e86dba78e
ErikaZanin <erikaczbu@gmail.com>
2025-04-06 15:55:03 -0300

    Iniciando projeto

---
+++ main.js
@@ -0,0 +1,69 @@
+function jogar(){
+    //Lista com 3 jogados escolhidos e vilões / Forças dos personagens 
+    personagemUsuario = [" "," "," "]
+    viloesComputador = [" "," "," "]
+    
+    forcapersonagemUsuario = 0
+    forcaviloesComputador = 0 
+     
+    
+    //Calcular força de cada jogador, e depois somar para saber a força do time
+    for(let i=0; i<3; i++){
+      escolhaPersonagem = prompt ("Digite o nome do seu personagem: " + (i + 1))
+      personagemUsuario[i] = escolhaPersonagem
+      //Calcular a força de cada jogador do time do computador 
+      forcapersonagemUsuario += Math.floor(Math.random() * 10) + 1 
+    }
+    alert("Agora seus personagens são: " + personagemUsuario)
+    
+    for(let i=0; i<3; i++){
+      indiceAleatorio = Math.floor(Math.random() * 5)
+      viloesPossiveis = ["Mojo Jojo","HIM (Ele)","Fuzzy Confusão","Princesa Maisgrana","Gangue Gangrena"]
+      viloesComputador[i] = viloesPossiveis[indiceAleatorio]
+      //Calcular a força de cada jogador do time do computador 
+      forcaviloesComputador += Math.floor(Math.random() * 10) + 1
+    }
+    
+    alert("Viloes: "+ viloesComputador)
+    
+    //Comparar os dois times para saber quem vencenu
+    
+       ganhadorID = 0
+       
+      //1 usuario
+      //2 computador 
+       //3 empate
+    if(forcapersonagemUsuario > forcaviloesComputador) {
+      ganhadorID = 1
+      alert("Você ganhou! Sua força: "+forcapersonagemUsuario+" Força do time vilão: "+forcaviloesComputador)
+    }
+    else{
+      if (forcapersonagemUsuario < forcaviloesComputador){
+        ganhadorID = 2
+        alert("Você perdeu! Sua força: "+forcapersonagemUsuario+" Força do time vilão: "+forcaviloesComputador)
+      }
+      else{
+        ganhadorID = 3
+        alert("Empate! Sejam amigos! Sua força: "+forcapersonagemUsuario+" Força do time vilão: "+forcaviloesComputador)
+      }
+    }
+      
+     ganhadorNome = ""
+     if(ganhadorID ==1){
+       ganhadorNome = "Time SuperPoderosas" 
+     }
+      else{
+        if(ganhadorID == 2){
+          ganhadorNome = "Time vilões"
+        }
+        else {
+          ganhadorNome = "Empate, sejam amigos!"
+        }
+      }
+      
+      document.getElementById('timeUsuario').innerText = personagemUsuario;
+      document.getElementById('timeComputador').innerText = viloesComputador;
+      document.getElementById('resultadoBatalha').innerText = ganhadorNome;
+    }
+    
+    
